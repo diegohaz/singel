030ad2b4fa65a410ddeed224c2af04301fdce330
Diego Haz <hazdiego@gmail.com>
2018-06-02 18:16:02 -0300

    Fix event handlers

--- src/testComponent.js
+++ src/testComponent.js
@@ -7,6 +7,8 @@ import { flow, camelCase, omit } from "lodash";
 import sinon from "sinon";
 import voidElements from "void-elements";
 import { all as cssProps } from "known-css-properties";
+import htmlProps from "react-known-props";
+import ariaProps from "aria-attributes/index.json";
 import { findHTMLTags, getHTMLTag, findHTMLTag } from "./utils";
 
 type TestFn = (Element: ComponentType<any>) => ComponentType<any>;
@@ -16,6 +18,16 @@ configure({ adapter: new Adapter() });
 const styleProps = cssProps
   .filter(prop => !/^-/.test(prop))
   .map(camelCase)
+  .reduce(
+    (acc, prop) => ({
+      ...acc,
+      [prop]: prop
+    }),
+    {}
+  );
+
+const reactProps = [...htmlProps, ...ariaProps]
+  .filter(prop => !/^style|className|on[A-Z].+$/.test(prop))
   .reduce(
     (acc, prop) => ({
       ...acc,
@@ -59,15 +71,18 @@ const testChildren: TestFn = Element => {
 };
 
 const testHTMLProps: TestFn = Element => {
-  // test every html prop
-  // test every svg prop if the component is svg
-  const wrapper = mount(<Element id="foo" />);
-  const tag = getHTMLTag(wrapper);
-  if (tag && !findHTMLTag(wrapper).find("[id='foo']").length) {
-    throw new Error(
-      `${Element.displayName || Element.name} should render html props.`
-    );
-  }
+  const wrapper = mount(<Element {...reactProps} />);
+  if (!getHTMLTag(wrapper)) return Element;
+  const props = findHTMLTag(wrapper).props();
+
+  Object.keys(reactProps).forEach(prop => {
+    if (props[prop] !== reactProps[prop]) {
+      throw new Error(
+        `${Element.displayName ||
+          Element.name} should render html prop (${prop}).`
+      );
+    }
+  });
   return Element;
 };
 
@@ -78,7 +93,7 @@ const testClassName: TestFn = Element => {
     findHTMLTag(originalWrapper).prop("className") || ""
   ).split(" ");
   const wrapper = mount(<Element className="bar" />);
-  const className = findHTMLTag(wrapper).prop("className");
+  const className = findHTMLTag(wrapper).prop("className") || "";
   const classNames = className.split(" ");
 
   if (!classNames.includes("bar")) {
@@ -126,17 +141,37 @@ const testStyle: TestFn = Element => {
 };
 
 const testEventHandlers: TestFn = Element => {
-  // test all the event handlers, not only onClick
-  // test for event arguments
-  const onClick = sinon.fake();
-  const wrapper = mount(<Element onClick={onClick} />);
+  const eventHandlers = htmlProps.filter(prop => /^on[A-Z]/.test(prop)).reduce(
+    (acc, prop) => ({
+      ...acc,
+      [prop]: sinon.stub()
+    }),
+    {}
+  );
+
+  const wrapper = mount(<Element {...eventHandlers} />);
   if (!getHTMLTag(wrapper)) return Element;
-  wrapper.simulate("click");
-  if (!onClick.called && !wrapper.getDOMNode().disabled) {
-    throw new Error(
-      `${Element.displayName || Element.name} should accept event handlers.`
-    );
-  }
+  Object.keys(eventHandlers).forEach(prop => {
+    const event = prop.replace(/^on/, "");
+    const lowercaseEvent = event.charAt(0).toLowerCase() + event.slice(1);
+    wrapper.simulate(lowercaseEvent);
+
+    if (!eventHandlers[prop].called) {
+      throw new Error(
+        `${Element.displayName ||
+          Element.name} should accept event handler (${prop}).`
+      );
+    }
+
+    const [arg] = eventHandlers[prop].getCall(0).args;
+
+    if (!arg || arg.constructor.name !== "SyntheticEvent") {
+      throw new Error(
+        `${Element.displayName ||
+          Element.name} should pass SyntheticEvent to ${prop}.`
+      );
+    }
+  });
   return Element;
 };
 
