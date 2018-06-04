dd4a9cdf380dc28d51e05337268979c64f358cf3
Raphael Thomazella <thomazella9@gmail.com>
2018-06-04 00:00:41 -0300

    WIP

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,9 @@
 import { join, isAbsolute } from "path";
 import meow from "meow";
 import { JSDOM } from "jsdom";
+import { upperCase, startCase } from "lodash";
 import testComponent from ".";
+import chalk from "chalk";
 
 const { window } = new JSDOM("<!doctype html><html><body></body></html>");
 global.window = window;
@@ -18,13 +20,45 @@ const cli = meow(`
 
 const run = paths => {
   require("babel-register")({ plugins: ["transform-es2015-modules-commonjs"] });
+  let result;
 
   paths.forEach(path => {
     const p = isAbsolute(path) ? path : join(process.cwd(), path);
     const { default: Component } = require(p);
-    const ret = testComponent(Component);
-    console.log(Object.keys(ret));
+    result = testComponent(Component);
   });
+  return result;
 };
 
-run(cli.input);
+const print = result => {
+  Object.keys(result).forEach(errorCategory => {
+    const prettyCategory = upperCase(errorCategory);
+    console.group(
+      "\n",
+      "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ",
+      "\n",
+      `***    ${prettyCategory}    ***`,
+      "\n",
+      "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ "
+    );
+
+    Object.keys(result[errorCategory]).forEach(errorType => {
+      const prettyType = startCase(errorType);
+      console.group("\n", `*  ${prettyType}  *`, "\n");
+      console.group();
+
+      if (result[errorCategory][errorType].length) {
+        result[errorCategory][errorType].forEach(error => console.log(error));
+      } else {
+        console.log(`✔︎ None`);
+      }
+
+      console.groupEnd("\n");
+      console.groupEnd();
+    });
+    console.groupEnd("\n\n");
+  });
+  console.log("\n\n");
+};
+
+print(run(cli.input));
