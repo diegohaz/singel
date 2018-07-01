e5451bca8913d7fd3953ae87b35fdb589dad3f1d
Diego Haz <hazdiego@gmail.com>
2018-07-01 19:03:29 -0300

    ccc

--- test/fixture.js
+++ test/fixture.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 // eslint-disable-next-line
-const Fixture = ({ children }) => (
-  <div {...props}>{children}</div>
+const Fixture = ({ children, ...props }) => (
+  <div>{children}</div>
 );
 
 export default Fixture;
